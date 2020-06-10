9717cb8c1af7cf0cc4d96e844223efa8d1db22e3
totoropcbeta <957584602@qq.com>
2020-06-10 08:13:31 +0800

    完成新建联系人测试

--- PageObject/contactpage.py
+++ PageObject/contactpage.py
@@ -1,16 +1,24 @@
 from PageObject.basepage import BasePage
+from time import sleep
 
 
 class ContactPage(BasePage):
     """邮箱新建联系人Page"""
     def new_contact(self):
+        """点击通讯录"""
+        sleep(5)
+        el_txl = self.by_xpath("//li[@title = '通讯录']")
+        el_txl.click()
         """点击新建联系人"""
+        sleep(5)
         el_new = self.by_xpath("//span[text() = '新建联系人']")
-        el_new.clicl()
+        el_new.click()
 
     def contact_msg(self, name, email, star, phone, note):
         """联系人信息"""
-        self.by_xpath("//*[@title = '编辑详细姓名']/preceding-sibling::div/input'").send_keys(name)
+        sleep(5)
+        el_name = self.by_xpath("//*[@title = '编辑详细姓名']/preceding-sibling::div/input")
+        el_name.send_keys(name)
         self.by_xpath("//*[@id = 'iaddress_MAIL_wrap']//input").send_keys(email)
         if star:
             self.by_xpath("//span[text() = '设为星标联系人']/preceding-sibling::span/b").click()
--- TestCase/test_contact.py
+++ TestCase/test_contact.py
@@ -2,6 +2,7 @@ import unittest
 from TestAction.login import Login
 from TestAction.new_contact import NewContact
 from selenium import webdriver
+from time import sleep
 
 
 class Testcontactcase(unittest.TestCase):
@@ -9,20 +10,43 @@ class Testcontactcase(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.driver = webdriver.Chrome()
-        log = Login()
-        log.login("wangyiwebtest", "wangyi2020")
+        cls.driver.maximize_window()
+        log = Login(cls.driver)
+        log.open()
+        log.login("totoropcbeta", "totoro520jcl")
         print("new contact test start.")
 
     @classmethod
     def tearDownClass(cls):
+        sleep(2)
+        cls.driver.quit()
         print("new contact test finished.")
 
     def test_001(self):
         """全部填写"""
         con = NewContact(self.driver)
         con.newcontact("张三", "85858685@163.com", True, "13285698745", "联系人1")
-        data = con.get_text("xpath", "//*[text() = '85858685@163.com']")
-        self.assertEqual("85858685@163.com", data)
+        self.assertIn("85858685@163.com", self.driver.page_source)
+
+    def test_002(self):
+        """不选星标"""
+        con = NewContact(self.driver)
+        con.newcontact("李四", "6848668486@163.com", False, "15964258963", "联系人2")
+        self.assertIn("6848668486@163.com", self.driver.page_source)
+
+    def test_003(self):
+        """不填邮件地址"""
+        con = NewContact(self.driver)
+        con.newcontact("王五", "", False, "18654235874", "联系人3")
+        self.assertIn("请正确填写邮件地址。", self.driver.page_source)
+        self.driver.refresh()
+
+    def test_004(self):
+        """填写错误邮件地址"""
+        con = NewContact(self.driver)
+        con.newcontact("钱六", "957584565@", False, "159564236985", "联系人4")
+        self.assertIn("请正确填写邮件地址。", self.driver.page_source)
+        self.driver.refresh()
 
 
 if __name__ == '__main__':
