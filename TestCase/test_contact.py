861105cbd1db0af8228a10e46e34383d3d7fd741
totoropcbeta <44626635+totoropcbeta@users.noreply.github.com>
2020-06-19 12:33:19 +0800

    添加联系人

--- TestCase/test_contact.py
+++ TestCase/test_contact.py
@@ -18,7 +18,11 @@ class Testcontactcase(unittest.TestCase):
     """163新建联系人测试"""
     @classmethod
     def setUpClass(cls):
-        cls.driver = webdriver.Chrome()
+        option = webdriver.ChromeOptions()
+        option.add_argument('headless')
+        option.add_argument('no-sandbox')
+        option.add_argument('disable-dev-shm-usage')
+        cls.driver = webdriver.Chrome('/usr/bin/chromedriver', chrome_options=option)
         cls.driver.maximize_window()
         log = Login(cls.driver)
         log.open()
